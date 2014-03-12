46083e6f6eaab0a532b0dd62c94965df68b7c961
Karl Glaser <kg@papercloud.com.au>
2014-03-13 01:44:05 +1100

    lexer

--- js/lexer.js
+++ js/lexer.js
@@ -1,1 +1,18 @@
-console.log('lexer')
+var lodash = require('../bower_components/lodash/dist/lodash');
+exports.isOperator = function(c) {
+    return /[+\-*\/\^%=(),]/.test(c);
+}
+exports.isDigit = function(c) {
+    return /[0-9]/.test(c);
+}
+exports.isWhiteSpace = function(c) {
+    return /\s/.test(c);
+}
+exports.isIdentifier = function(c) {
+    // return typeof c === "string" && !isOperator(c) && !isDigit(c) && !isWhiteSpace(c);
+}
+exports.lex = function(input) {
+    return lodash.map(input, function(c) {
+        return c;
+    });
+};
--- tests/lexer.js
+++ tests/lexer.js
@@ -1,5 +1,16 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
+var lexer = require('../js/lexer.js');
+describe("Lexer", function() {
+    it("regognizer", function() {
+        expect(lexer.isOperator('*')).toBe(true);
+        expect(lexer.isOperator(' *')).toBe(true);
+        expect(lexer.isWhiteSpace(' ')).toBe(true);
+        expect(lexer.isWhiteSpace(' *')).toBe(true);
+        expect(lexer.isWhiteSpace('\
+        	')).toBe(true);
+        expect(lexer.isWhiteSpace('\n')).toBe(true);
+        expect(lexer.isDigit('1')).toBe(true);
+    });
+    it("builder", function() {
+        expect(lexer.lex("ab")).toEqual(['a', 'b']);
+    });
 });
