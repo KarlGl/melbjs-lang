b0b7e15cfa490fc4ebd5d8b4cbb5db4ab1d20ab0
Karl Glaser <kg@papercloud.com.au>
2014-03-28 02:21:23 +1100

    a

--- tests/core.js
+++ tests/core.js
@@ -1,4 +1,5 @@
 var app = require('../js/core.js');
+var sinon = require('../bower_components/sinon/lib/sinon.js');
 
 describe('Full stack', function() {
     it('assignment', function() {
@@ -20,33 +21,30 @@ describe('Full stack', function() {
     it('else', function() {
         expect(app.run('(?,false,1,0)')).toEqual(0)
     });
+    it('if statement will evaluate the active branch', function() {
+        spy = sinon.spy(app.subroutines.functions, '=');
+        app.run('(?,true,(=,1,1),1')
+        // assigning 1 to 1 should not be processed, it's on a false branch!
+        expect(spy.callCount).toEqual(1)
+        spy.restore();
+    });
     it('if statement wont evaluate the non active branch', function() {
-        var called = false;
-        // TODO replace with sinon... this doesn't unstub
-        // mock
-        app.subroutines.functions['='] = function() {
-            called = true;
-        }
+        spy = sinon.spy(app.subroutines.functions, '=');
         app.run('(?,true,1,(=,1,1)')
         // assigning 1 to 1 should not be processed, it's on a false branch!
-        expect(called).toEqual(false)
+        expect(spy.callCount).toEqual(0)
+        spy.restore();
     });
     it('define and call function', function() {
         expect(app.run('(.,(`,(+,1,1)))')).toEqual(2);
     });
     it('wont call function body when definied', function() {
-        var called = false;
-        // mock
-        old = app.subroutines.functions['+'];
-        app.subroutines.functions['+'] = function() {
-            called = true;
-        }
+        spy = sinon.spy(app.subroutines.functions, '+');
         app.run('(`,(+,1,1))')
-        expect(called).toEqual(false)
-        app.subroutines.functions['+'] = old;
+        expect(spy.callCount).toEqual(0)
+        app.subroutines.functions['+'].restore();
     });
     it('define function with argument', function() {
-        console.log(app.run('(.,(`,(+,(*,a),1),a),2)'))
-        expect(3).toEqual(3);
+        expect(app.run('(.,(`,(+,(*,a),1),a),2)')).toEqual(3);
     });
 })
